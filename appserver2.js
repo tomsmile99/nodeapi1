6e3181187bcf8046286314678ec02fd80c49fb80
tomsmile99 <babyking2532@gmail.com>
2023-09-21 10:12:57 +0700

    first commit

---
+++ appserver2.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const socketIO = require('socket.io')
+const cors = require('cors')
+
+
+// you explicitly create the http server
+//const httpServer = require("http").createServer(app);
+
+// you explicitly create the http server
+//const server = require('http').createServer(app);
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors({
+  origin: '*'
+}))
+
+require('dotenv').config()
+
+//const hostname = process.env.HOST
+const port = process.env.PORT || 3000
+
+
+app.get('/', (req, res) => {
+  res.send('มาละๆๆ');
+})
+
+
+const server = app.listen(port, () => {
+  console.log(`Server running at port : ${port}/`);
+})
+
+const io = socketIO(server)
+io.on('connection',(socket)=>{
+  console.log('client socket connection')
+})
